2af0c25afdd01bead0f3684e488b7b410e93b751
MutaiAllan <allankiprop175@gmail.com>
2024-03-20 16:34:27 +0300

    Added langingPage function

--- src/MutaisATM/Main.java
+++ src/MutaisATM/Main.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Account Mutai = new Account("Mutai", "234234");
-        Account Mutai1 = new Account("Mutai1", "2342");
+        // Account Mutai = new Account("Mutai", "234234");
+        // Account Mutai1 = new Account("Mutai1", "2342");
 
-        System.out.println(Mutai.getName());
-        System.out.println(Mutai.getBalance());
+        // System.out.println(Mutai.getName());
+        // System.out.println(Mutai.getBalance());
 
-        System.out.println(Mutai1.getName());
+        // System.out.println(Mutai1.getName());
 
-        Mutai.transfer("Nancy", 40);
+        // Mutai.transfer("Nancy", 40);
 
-        System.out.println(Mutai.getBalance());
+        // System.out.println(Mutai.getBalance());
 
-        Mutai.getAccountDetails(Mutai1);
+        // Mutai.getAccountDetails(Mutai1);
+
+        landingPage();
 
     }
+
+
+    public static void landingPage(){
+            System.out.println("Welcome to Mutai's ATM!");
+            System.out.println();
+            System.out.println("1. Create Account");
+            System.out.println("2. Login");
+            System.out.println("3. Exit");
+            System.out.println();
+
+            System.out.print("Enter your choice: ");
+        }
 }

8ce17818955d189a42da0d782ac93feeb19d53ef
MutaiAllan <allankiprop175@gmail.com>
2024-03-20 15:56:56 +0300

    Added initial files

---
+++ src/MutaisATM/Account.java
@@ -0,0 +1,20 @@
+public class Account {
+    private String name;
+    private String accNo;
+    private int balance;
+
+    public Account (String name, String accNo) {
+        this.name = name;
+        this.accNo = accNo;
+        this.balance = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+}
---
+++ src/MutaisATM/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Account Mutai = new Account("Mutai", "234234");
+
+        System.out.println(Mutai.getName());
+        System.out.println(Mutai.getBalance());
+
+        
+
+    }
+}
